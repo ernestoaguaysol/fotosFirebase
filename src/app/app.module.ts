8a37e6afbc1415023985cafa5bd7ce7349f8bcf5
Ernesto <ernestoaguaysol@gmail.com>
2018-01-25 14:55:21 -0300

    model and initial config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FotosComponent } from './components/fotos/fotos.component';
+import { CargaComponent } from './components/carga/carga.component';
+
+// routes
+import { AppRouting } from './app.routes';
+
+// Services
+import { CargaImagenesService } from './services/carga-imagenes.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FotosComponent,
+    CargaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouting
   ],
-  providers: [],
+  providers: [
+    CargaImagenesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
