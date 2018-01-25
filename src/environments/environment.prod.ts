3c06e752a5575e04f9e8d9679b83398a1e206717
Ernesto <ernestoaguaysol@gmail.com>
2018-01-25 20:48:13 -0300

    produ

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyASRe3QoKokB1P0pFOP-oGliqbMo9KiuIU',
+    authDomain: 'fir-fotos-d9bb5.firebaseapp.com',
+    databaseURL: 'https://fir-fotos-d9bb5.firebaseio.com',
+    projectId: 'fir-fotos-d9bb5',
+    storageBucket: 'fir-fotos-d9bb5.appspot.com',
+    messagingSenderId: '198418879234'
+  }
 };
